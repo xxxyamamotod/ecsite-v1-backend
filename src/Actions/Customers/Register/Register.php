80bfed13864083f68d9ee146714ed05764d8d1ef
yamamoto_d <d.yamamoto@xxxaz.jp>
2022-06-26 14:39:21 +0900

    [update] ValidatorのインターフェースであるIValidator作成
    安全にDIを行うため

--- src/Actions/Customers/Register/Register.php
+++ src/Actions/Customers/Register/Register.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Actions\Customers\Register;
 
 use App\Actions\Customers\Register\ValidationResult;
-use App\Actions\Customers\Register\Validator;
 use App\Model\Table\CustomersTable;
 use Cake\Http\Exception\BadRequestException;
+
+interface IValidator
+{
+    public function result(bool $isPost);
+}
 
 /**
  * App\Controller\CustomersController内 registerメソッド用
@@ -15,16 +19,20 @@ use Cake\Http\Exception\BadRequestException;
 class Register
 {
     private CustomersTable $customersTable;
+    private IValidator $validator;
 
     /**
      * DI注入
      *
      * @param CustomersTable $customersTable
+     * @param Validator $validator
      */
     public function __construct(
-        CustomersTable $customersTable
+        CustomersTable $customersTable,
+        IValidator $validator
     ) {
         $this->customersTable = $customersTable;
+        $this->validator = $validator;
     }
 
     /**
@@ -36,7 +44,7 @@ class Register
      */
     public function action(bool $isPost, array $requests): ValidationResult
     {
-        $validationResult = Validator::result($isPost);
+        $validationResult = $this->validator->result($isPost);
         if (!$validationResult->isValid()) {
             throw new BadRequestException($validationResult->getMessage());
         }
--- src/Actions/Customers/Register/Validator.php
+++ src/Actions/Customers/Register/Validator.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Actions\Customers\Register;
 
 use App\Actions\Customers\Register\ValidationResult;
+use App\Actions\Customers\Register\IValidator;
 
 /**
  * App\Actions\Customers\Registerのバリデータ
  */
-class Validator
+class Validator implements IValidator
 {
     /**
      * バリデーションの結果を返してくれる
@@ -17,7 +18,7 @@ class Validator
      * @param boolean $isPost
      * @return ValidationResult
      */
-    public static function result(bool $isPost): ValidationResult
+    public function result(bool $isPost): ValidationResult
     {
         if (!$isPost) {
             return ValidationResult::inValid('不正なリクエストです。');
